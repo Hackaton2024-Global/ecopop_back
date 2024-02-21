4faefe8a776f28646c596e7d952e852df2d86aaf
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-21 12:08:00 +0100

    first commit

---
+++ src/main/java/ecopopHackaton/ecopop_back/EcopopBackApplication.java
@@ -0,0 +1,13 @@
+package ecopopHackaton.ecopop_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcopopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcopopBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecopopHackaton/ecopop_back/EcopopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ecopopHackaton.ecopop_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcopopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
